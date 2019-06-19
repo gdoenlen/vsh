c783a516074765f2cc0cf08c46c8c12f7f414950
George Doenlen <gdoenlen@gmail.com>
2019-06-18 20:35:48 -0400

    clean up unused vars

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,6 @@
 #include <vsh.hpp>
 #include <winapi.hpp>
 
-using std::getenv;
-using std::vector;
-using std::wstring;
-using std::stringstream;
-using std::string;
-using std::move;
-using std::cout;
-using std::make_unique;
-using std::make_shared;
-using std::getline;
 using winapi::WindowsApiService;
 using namespace vsh;
 
@@ -30,27 +20,20 @@ using namespace vsh;
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 int main(int argc, char** argv) {
     if (argc > 1) {
-        cout << "Vip's Shell v0.1\n";
-        cout << "usage: vsh\n";
+        std::cout << "Vip's Shell v0.1\n";
+        std::cout << "usage: vsh\n";
         return 0;
     } 
 
-    auto path = string(getenv("PATH"));
-    auto vec = make_shared<vector<string>>(); 
-    auto ss = stringstream(path);
-    string to;
-    while (getline(ss, to, ';')) {
-        vec->push_back(move(to));
-    }
     HANDLE hin = GetStdHandle(STD_INPUT_HANDLE);
     auto winapi = WindowsApiService();
-    auto buffer = make_unique<wchar_t[]>(256);
+    auto buffer = std::make_unique<wchar_t[]>(256);
 
     // for now we need to use windows.h to get the cwd
     // mingw doesn't have proper support for std::filesystem
     winapi.get_cwd(buffer.get(), 256);
-    auto cwd = wstring(buffer.get());
-    auto vsh = make_unique<Vsh>(move(cwd), move(hin), move(winapi)); 
+    auto cwd = std::wstring(buffer.get());
+    auto vsh = std::make_unique<Vsh>(std::move(cwd), std::move(hin), std::move(winapi)); 
 
     vsh->start();
 
